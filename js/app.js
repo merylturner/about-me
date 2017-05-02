75bd14e34348daf000c79b00fbab77f8005cd355
Meryl Turner <merylturner08@gmail.com>
2017-05-02 14:30:43 -0700

    add files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+var name = confirm("Is my name Meryl Turner? ");
+console.log(name.toUpperCase());
+var live = confirm("Do I live in Portland, Oregon?");
+var outdoors = confirm("Do I like the outdoors?");
+var coffee = confirm("Do I drink too much coffee?");
+var coding = confirm("Do I love to code?");
