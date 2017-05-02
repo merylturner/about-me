edf4854095a0567da0d302d802b48392d0a4c09a
Meryl Turner <merylturner08@gmail.com>
2017-05-02 15:36:06 -0700

    modified css styles and finalized html

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 'use strict';
 var name = prompt('Is my name Meryl Turner? ');
-if (name.toUpperCase () === 'YES' || name.toUpperCase () === 'N') {
+if (name.toUpperCase () === 'YES' || name.toUpperCase () === 'Y') {
 	alert('Yes, my name is Meryl!');
 }
 else{
-	alert('No, my name is not Meryl.');
+	alert('No, my name is Fred.');
 }
-Console.log('1st ?' + name);
-var born = prompt('Was I born in Portland, Oregon');
+console.log('1st Question: ' + name.toUpperCase());
+var born = prompt('Was I born in Portland, Oregon?');
 if(born.toLowerCase () === 'yes' || born.toLowerCase () === 'y') {
 	alert('Yes, I was born in Portland, Oregon!');
 }
 else {
 	alert('No, I was not born in Portland, Oregon.');
 }
-Console.log('2nd ?' + born);
+console.log('2nd Question: ' + born.toLowerCase());
 var outdoors = prompt('Do I like the Outdoors?');
 if(outdoors.toUpperCase () === 'YES' || outdoors.toUpperCase () === 'Y') {
 	alert('Heck yes! We are in the Pacific Northwest');
@@ -22,20 +22,20 @@ if(outdoors.toUpperCase () === 'YES' || outdoors.toUpperCase () === 'Y') {
 else {
 	alert('No I prefer to stay indoors at all times.');
 }
-Console.log('3rd ?' + outdoors);
+console.log('3rd Question: ' + outdoors.toUpperCase());
 var coffee = prompt('Do I drink too much coffee?');
-    if (coffee.toLowerCase() === 'yes' || coffee.toLowerCase () === 'y') {
-        alert('NO WAY CAFFEINE IS MY FRIEND');
-    }
-    else {
-        alert('Is there such a thing as too much coffee?');
-    }
-Console.log('4th ?' + coffee);
-var coding = prompt('Do I love to code?');
-if (coding.toUpperCase() === 'YES' || coding.toUpperCase() === 'Y') {
-    alert('Yes! Coding is the best thing ever!');
+if (coffee.toLowerCase() === 'yes' || coffee.toLowerCase () === 'y') {
+	alert('NO WAY CAFFEINE IS MY FRIEND');
 }
 else {
-    alert('No I want to throw my computer against the wall.');
+	alert('Is there such a thing as too much coffee?');
 }
-Console.log('5th ?' + coding);
+console.log('4th Question: ' + coffee.toLowerCase());
+var coding = prompt('Do I love to code?');
+if (coding.toUpperCase() === 'YES' || coding.toUpperCase() === 'Y') {
+	alert('Yes! Coding is the best thing ever!');
+}
+else {
+	alert('No I want to throw my computer against the wall.');
+}
+console.log('5th Question: ' + coding.toUpperCase());
